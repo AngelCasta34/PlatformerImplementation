68115d55da3e63c57c452c2fe5497cb122c2871d
Angel <162243536+AngelCasta34@users.noreply.github.com>
2025-05-18 16:55:56 -0700

    Update Platformer.js

--- PlatformerImplementation/src/Scenes/Platformer.js
+++ PlatformerImplementation/src/Scenes/Platformer.js
@@ -1,11 +1,11 @@
-class Platformer extends Phaser.Scene { 
+class Platformer extends Phaser.Scene {
     constructor() {
         super("platformerScene");
     }
 
     init() {
         this.ACCELERATION      = 100;
-        this.DRAG              = 500;
+        this.DRAG              = 1800;
         this.physics.world.gravity.y = 1500;
         this.JUMP_VELOCITY     = -400;
         this.PARTICLE_VELOCITY = 50;
@@ -13,57 +13,69 @@ class Platformer extends Phaser.Scene {
     }
 
     create() {
-        const TILE_W = 9;
-        const TILE_H = 9;
-
+        // 1) TILEMAP + GROUND
+        const TILE_W = 9, TILE_H = 9;
         this.map = this.add.tilemap("platformer-level-1", TILE_W, TILE_H, 45, 25);
         this.tileset = this.map.addTilesetImage("kenny_tilemap_packed", "tilemap_tiles");
         this.groundLayer = this.map.createLayer("Ground-n-Platforms", this.tileset, 0, 0);
         this.groundLayer.setCollisionByProperty({ collides: true });
 
-        this.coins = this.map.createFromObjects("Objects", {
-            name: "coin",
-            key:  "tilemap_sheet",
-            frame: 151
+        // 2) COINS
+        this.coinGroup = this.physics.add.staticGroup();
+        const coinObjects = this.map.getObjectLayer("Objects").objects
+            .filter(obj => obj.name === "coin" && obj.gid);
+        const firstGid = this.map.tilesets[0].firstgid;
+        coinObjects.forEach(obj => {
+            const frameIndex = obj.gid - firstGid;
+            const coin = this.coinGroup.create(
+                obj.x + TILE_W/2,
+                obj.y,
+                "tilemap_sheet",
+                frameIndex
+            );
+            coin.setOrigin(0.5, 1);
         });
-        this.physics.world.enable(this.coins, Phaser.Physics.Arcade.STATIC_BODY);
-        this.coinGroup = this.add.group(this.coins);
 
-        const spawnPoint = this.map.findObject("Objects", obj => obj.name === "Spawn");
-
-        this.my = { sprite:{}, vfx:{} };
+        // 3) PLAYER
+        const spawn = this.map.findObject("Objects", o => o.name === "Spawn");
+        this.my = { sprite: {}, vfx: {} };
         this.my.sprite.player = this.physics.add
-            .sprite(spawnPoint.x, spawnPoint.y, "platformer_characters", "tile_0000.png")
+            .sprite(spawn.x, spawn.y, "platformer_characters", "tile_0000.png")
             .setOrigin(0.5, 1)
             .setScale(0.5)
             .setCollideWorldBounds(true);
 
+        // Collide & overlap
         this.physics.add.collider(this.my.sprite.player, this.groundLayer);
         this.physics.add.overlap(
             this.my.sprite.player,
             this.coinGroup,
-            (player, coin) => coin.destroy()
+            (player, coin) => {
+                coin.destroy();
+                this.sound.play("sfx-coin");  
+            }
         );
 
-        // Key bindings
+        // 4) INPUT
         this.aKey     = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.dKey     = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-        this.rKey = this.input.keyboard.addKey('R');
-        this.input.keyboard.on('keydown-D', () => {
+        this.rKey     = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        this.fKey     = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        this.input.keyboard.on('keydown-F', () => {
             this.physics.world.drawDebug = !this.physics.world.drawDebug;
             this.physics.world.debugGraphic.clear();
-        }, this);
+        });
 
-        // **Scaled-down smoke particles**
+        // 5) WALK VFX
         this.my.vfx.walking = this.add.particles(0, 0, "kenny-particles", {
-            frame:   ['smoke_03','smoke_09'],
-            scale:   { start: 0.015, end: 0.05 },  // ← half the size
+            frame:    ['smoke_03','smoke_09'],
+            scale:    { start: 0.015, end: 0.05 },
             lifespan: 350,
-            alpha:   { start: 1, end: 0.1 },
+            alpha:    { start: 1, end: 0.1 },
         }).stop();
 
+        // 6) CAMERA
         this.cameras.main
             .setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
             .startFollow(this.my.sprite.player, true, 0.25, 0.25)
@@ -75,11 +87,12 @@ class Platformer extends Phaser.Scene {
         const p   = this.my.sprite.player;
         const vfx = this.my.vfx.walking;
 
+        // left/right
         if (this.aKey.isDown) {
             p.setAccelerationX(-this.ACCELERATION);
             p.resetFlip();
             p.anims.play('walk', true);
-            vfx.startFollow(p, p.displayWidth/2 - 10, p.displayHeight/2 - 5, false);
+            vfx.startFollow(p, p.displayWidth/2 - 10, p.displayHeight/2 - 5);
             vfx.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
             if (p.body.blocked.down) vfx.start();
 
@@ -87,7 +100,7 @@ class Platformer extends Phaser.Scene {
             p.setAccelerationX(this.ACCELERATION);
             p.setFlip(true, false);
             p.anims.play('walk', true);
-            vfx.startFollow(p, -p.displayWidth/2 + 10, p.displayHeight/2 - 5, false);
+            vfx.startFollow(p, -p.displayWidth/2 + 10, p.displayHeight/2 - 5);
             vfx.setParticleSpeed(-this.PARTICLE_VELOCITY, 0);
             if (p.body.blocked.down) vfx.start();
 
@@ -98,16 +111,14 @@ class Platformer extends Phaser.Scene {
             vfx.stop();
         }
 
-        if (!p.body.blocked.down) {
-            p.anims.play('jump');
-        }
+        // jump
+        if (!p.body.blocked.down) p.anims.play('jump');
         if (p.body.blocked.down && Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
             p.body.setVelocityY(this.JUMP_VELOCITY);
         }
 
-        if (Phaser.Input.Keyboard.JustDown(this.rKey)) {
-            this.scene.restart();
-        }
+        // restart
+        if (Phaser.Input.Keyboard.JustDown(this.rKey)) this.scene.restart();
     }
 }
 
