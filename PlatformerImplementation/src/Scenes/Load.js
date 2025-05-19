bb13f1b2040a2b6bb1365620f33ac771d4d0689b
Angel <162243536+AngelCasta34@users.noreply.github.com>
2025-05-19 11:53:45 -0700

    Update Load.js

--- PlatformerImplementation/src/Scenes/Load.js
+++ PlatformerImplementation/src/Scenes/Load.js
@@ -1,65 +1,64 @@
 class Load extends Phaser.Scene {
-    constructor() {
-        super("loadScene");
-    }
+constructor() {
+    super("loadScene");
+}
 
-    preload() {
-        this.load.setPath("./assets/");
+preload() {
+    this.load.setPath("./assets/");
 
-        // Load characters spritesheet
-        this.load.atlas("platformer_characters", "tilemap-characters-packed.png", "tilemap-characters-packed.json");
+    // Load characters spritesheet
+    this.load.atlas("platformer_characters", "tilemap-characters-packed.png", "tilemap-characters-packed.json");
 
-        // Load tilemap information
-        this.load.image("tilemap_tiles", "tilemap_packed.png");                         // Packed tilemap
-        this.load.tilemapTiledJSON("platformer-level-1", "platformer-level-1.tmj");   // Tilemap in JSON
+    // Load tilemap information
+    this.load.image("tilemap_tiles", "tilemap_packed.png");                         // Packed tilemap
+    this.load.tilemapTiledJSON("platformer-level-1", "platformer-level-1.tmj");   // Tilemap in JSON
 
-        // Load the tilemap as a spritesheet
-        this.load.spritesheet("tilemap_sheet", "tilemap_packed.png", {
-            frameWidth: 9,
-            frameHeight: 9
-        });
+    // Load the tilemap as a spritesheet
+    this.load.spritesheet("tilemap_sheet", "tilemap_packed.png", {
+        frameWidth: 9,
+        frameHeight: 9
+    });
 
-        this.load.multiatlas("kenny-particles", "kenny-particles.json");
+    this.load.multiatlas("kenny-particles", "kenny-particles.json");
 
-        this.load.audio("sfx-coin", "forceField_000.ogg");
-    }
+    this.load.audio("sfx-coin", "forceField_000.ogg");
+}
 
-    create() {
-        this.anims.create({
-            key: 'walk',
-            frames: this.anims.generateFrameNames('platformer_characters', {
-                prefix: "tile_",
-                start: 0,
-                end: 1,
-                suffix: ".png",
-                zeroPad: 4
-            }),
-            frameRate: 15,
-            repeat: -1
-        });
+create() {
+    this.anims.create({
+        key: 'walk',
+        frames: this.anims.generateFrameNames('platformer_characters', {
+            prefix: "tile_",
+            start: 0,
+            end: 1,
+            suffix: ".png",
+            zeroPad: 4
+        }),
+        frameRate: 15,
+        repeat: -1
+    });
 
-        this.anims.create({
-            key: 'idle',
-            defaultTextureKey: "platformer_characters",
-            frames: [
-                { frame: "tile_0000.png" }
-            ],
-            repeat: -1
-        });
+    this.anims.create({
+        key: 'idle',
+        defaultTextureKey: "platformer_characters",
+        frames: [
+            { frame: "tile_0000.png" }
+        ],
+        repeat: -1
+    });
 
-        this.anims.create({
-            key: 'jump',
-            defaultTextureKey: "platformer_characters",
-            frames: [
-                { frame: "tile_0001.png" }
-            ],
-        });
+    this.anims.create({
+        key: 'jump',
+        defaultTextureKey: "platformer_characters",
+        frames: [
+            { frame: "tile_0001.png" }
+        ],
+    });
 
-         // ...and pass to the next Scene
-         this.scene.start("platformerScene");
-    }
+        this.scene.start("platformerScene");
+}
 
-    // Never get here since a new scene is started in create()
-    update() {
-    }
+// Never get here since a new scene is started in create()
+update() {
+}
 }
